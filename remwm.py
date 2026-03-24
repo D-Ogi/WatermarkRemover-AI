ecfc683c7df1f6ceb3b033ac9cd6825914679a04
FeronFae <77190506+FeronFae@users.noreply.github.com>
2026-03-24 22:12:05 +0100

    Avoid CPU type conflicts by utilizing torch.float32 if invoked
    Had issues running the model on my AMD-only setup (meaning I was running it purely on CPU.) I found the lack of dtype declaration; I recommend running the float32 on CPUs in general to avoid type conflicts.

--- remwm.py
+++ remwm.py
@@ -658,7 +658,14 @@ def main(input_path: str, output_path: str, preview: bool, overwrite: bool, tran
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
-    florence_model = Florence2ForConditionalGeneration.from_pretrained("florence-community/Florence-2-large").to(device).eval()
+
+    # Force float32 for CPU compatibility
+    dtype = torch.float32 if device == "cpu" else torch.float16
+    florence_model = Florence2ForConditionalGeneration.from_pretrained(
+        "florence-community/Florence-2-large",
+        torch_dtype=dtype,
+        trust_remote_code=True
+        ).to(device).eval()
     florence_processor = AutoProcessor.from_pretrained("florence-community/Florence-2-large")
     logger.info("Florence-2 Model loaded")
 
